60fde00c0f07876201fa3d687ab939aa029bea74
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-07-22 04:24:35 +0000

    setting up graphiql

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-const express = require('express');
-require('dotenv').config();
-const app = express();
-const PORT = process.env.PORT || 4000;
+const express = require("express");
+require("dotenv").config();
+// const expressGraphQL = require("express-graphql");
+const { graphqlHTTP } = require("express-graphql");
 
-app.listen(PORT, ()=> console.log(`server listening on ${PORT}`));
+const app = express();
+
+app.use(
+  "/api",
+  graphqlHTTP({
+    schema: require("./schema/schema"),
+    graphiql: process.env.NODE_ENV === "development" ? true : false,
+  })
+);
+// app.use(
+//   "/api",
+//   expressGraphQL({
+//     schema: schema,
+//     graphiql: true,
+//   })
+// );
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`server listening on ${PORT}`));
