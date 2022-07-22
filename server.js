63b16b47470bdfc408a4a65faaea7aa2920ed44b
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-07-22 04:18:48 +0000

    added start and dev scripts

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, ()=> console.log(`server listening on ${PORT}`));
